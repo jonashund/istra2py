cc9872e563509620b973bf909c6de5398f9cd5c1
Julian Bauer <julian.bauer@kit.edu>
2020-06-12 20:54:13 +0200

    Add first script

---
+++ scripts/s010.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+np.set_printoptions(
+    linewidth=160,
+    precision=4,
+    # suppress=False,
+)
+
+import h5py
+import os
+
+path = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+    "data",
+    "series_step_0.hdf5",
+)
+
+f = h5py.File(path, "r")
+
+print(f.keys())
+print()
+
+coords = f["coordinates"]
+displacements = f["displacements"]
+strain = f["strains"]
+
+print(coords.keys())
+print()
+print(displacements.keys())
+print()
+print(strain.keys())
+print()
+
+import numpy as np
+
+matrix = strain["strain_p1"]
+val = np.array(matrix).flatten()
