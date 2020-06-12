15f9d573ac6d03dbf2d5df7ac14a33605d6f2228
Julian Bauer <julian.bauer@kit.edu>
2020-06-12 21:14:02 +0200

    Plot coordinates

--- istra2py.py
+++ istra2py.py
@@ -116,3 +116,25 @@ if __name__ == "__main__":
     # r.list_available_keys()
     r.get_basics()
 
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(1, 1, figsize=(45, 45))
+
+    plotable_slice = np.s_[:, 1:-2, 1:-2, :]
+
+    x = r.x[plotable_slice][0, :, :, 0]
+    y = r.x[plotable_slice][0, :, :, 1]
+    val = r.x[plotable_slice][-1, :, :, 1]
+
+    cs = ax.contourf(x, y, val, cmap="viridis",)
+
+    # Plot grid based on discretization
+    ax.plot(
+        x, y, "k-", lw=0.5, alpha=0.5,
+    )
+    ax.plot(
+        x.T, y.T, "k-", lw=0.5, alpha=0.5,
+    )
+
+    # ax.grid(c="k", ls="-", alpha=0.5)
+    fig.colorbar(cs, ax=ax, shrink=0.9, format="%.0e")
