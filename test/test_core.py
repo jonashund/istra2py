923609844035f34f304742fa761cd6927e11f266
Julian Bauer <julian.bauer@kit.edu>
2020-06-12 20:44:29 +0200

    Test init

--- test/test_core.py
+++ test/test_core.py
@@ -6,6 +6,7 @@
 
 import os
 import sys
+import pytest
 
 sys.path.append(os.path.join(".."))
 import istra2py
@@ -14,9 +15,25 @@ import istra2py
 ##############################################
 
 
+@pytest.fixture
+def reader():
+    return istra2py.Reader("data")
+
+
 class Test_core:
-    def test_init(self,):
-        istra2py.Reader("data")
+    def test_init(self, reader):
+        pass
+
+    def test_init_nonexisting_dir(self,):
+        with pytest.raises(FileNotFoundError):
+            istra2py.Reader("data_not_existing_124376124")
+
+    def test_init_empty_dir(self,):
+        with pytest.raises(istra2py.Istra2pyException):
+            istra2py.Reader("data_empty")
+
+    def test_list_available_keys(self, reader):
+        reader.list_available_keys()
 
 
 if __name__ == "__main__":
