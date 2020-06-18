1c1077a4fdf2816882afdf1c6956790613a5a2b7
Julian Bauer <julian.bauer@kit.edu>
2020-06-18 17:07:20 +0200

    Doc references setup.py tests

--- setup.py
+++ setup.py
@@ -8,19 +8,21 @@ setuptools.setup(
     description="Read hdf5 files exported from Istra4D digital image correlation with python",
     url="https://git.scc.kit.edu/ifm/werkstatt/istra2py",
     py_modules=["istra2py"],
+
+    install_requires=["h5py", "numpy"],
+    extras_require={'test': ["pytest", "matplotlib"]},
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     # packages=["istra2py"],
     # package_dir={"istra2py": "istra2py"},
     # package_data={"istra2py": ["data.bib"]},
     # scripts=["scripts/create_excel_view.py"],
     # include_package_data=True,
-    install_requires=["h5py", "numpy"],
-    extras_require={'test': ["pytest", "matplotlib"]},
     # tests_require=install_requires + ["pytest", "matplotlib"],
     # extras_require={
     #     ":python_version>'3.5'": ["pandas", "pybtex", "openpyxl", "matplotlib"]
     # },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
 )
