1b61d81aee70a323c598b57e411ae90c75afc667
JulianKarlBauer <55383008+JulianKarlBauer@users.noreply.github.com>
2022-07-13 10:07:41 +0200

    Update contact details

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 setuptools.setup(
     name="istra2py",
     version="0.0.1",
-    author="Julian Bauer, Jonas Hund",
-    author_email="julian.bauer@kit.edu, jonas.hund@kit.edu",
+    author="Julian Karl Bauer, Jonas Hund",
+    author_email="juliankarlbauer@gmx.de, jonas.hund@kit.edu",
     description="Read hdf5 files exported from Istra4D digital image correlation with python",
     url="https://git.scc.kit.edu/ifm/werkstatt/istra2py",
     py_modules=["istra2py"],
